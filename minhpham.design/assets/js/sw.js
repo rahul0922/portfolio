43290c247523102156825a5fa94b072aabc83cd2
rahul <grogu@rahuls-MacBook-Air.local>
2026-02-17 01:31:37 +0530

    Initial commit: Project setup with Bento Box layout, pricing updates, and assets

---
+++ minhpham.design/assets/js/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function (e) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+    self.registration.unregister();
+    clients.claim();
+});
